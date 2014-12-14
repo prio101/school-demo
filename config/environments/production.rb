884a03d3f6e1d183fbb65ad2b5c79f8acca83dd5
nurulshuvo <shuvo@e-solutionpark.com>
2014-12-14 15:31:41 +0600

    activeadmin assets precompile issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ SchoolWings::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico *.js *.css *.css.erb)
+  config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
 
   # Generate digests for assets URLs.
   config.assets.digest = true
